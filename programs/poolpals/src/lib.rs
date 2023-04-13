716886a5ce2410165ac159cf1afba28b4d0e949f
Andrew <andrewhayter@gmail.com>
2023-04-13 16:08:09 -0700

    moving other lotto work over to poolpals.

--- programs/poolpals/src/lib.rs
+++ programs/poolpals/src/lib.rs
@@ -1,123 +1,236 @@
-// mod error;
 use anchor_lang::prelude::*;
-use anchor_spl::token;
 
-pub mod error;
+declare_id!("4Et6MKLER9MBDCoKeV8xG3Ei2nxGRTW3sWNYmUcjGdmZ");
 
-use crate::error::ErrorCode;
+#[account]
+#[derive(InitSpace, Debug, PartialEq, Copy)]
+pub struct LotteryState {
+    authority: Pubkey,
+    lottery_id: u64,
+    status: LotteryStatus,
+    ticket_price: u64,
+    prize_pool: u64,
+    ticket_count: u64,
+    winner: Option<Pubkey>,
+    start_time: i64,
+    end_time: i64,
+    max_tickets: u64,
+}
 
-const AUTHORIZED_INITIATOR: &str = "Dvfu3r2iWSRri8bWdwwLQ5Krujogf3ZRauVRSXYRKAwN";
+#[account]
+#[derive(Debug, InitSpace)]
+pub struct LotteryVault {
+    lottery_id: u64,
+    authority: Pubkey,
+}
 
-declare_id!("FCy8iFxZmzpdqFhDh8nMEt5maHSCYPijAPqJonChehjJ");
+#[account]
+#[derive(Debug, InitSpace)]
+pub struct TicketData {
+    lottery_id: u64,
+    user: Pubkey,
+    ticket_count: u64,
+    total_deposited: u64,
+}
+
+#[derive(Debug, AnchorSerialize, AnchorDeserialize, PartialEq, Clone, Copy, InitSpace)]
+pub enum LotteryStatus {
+    Pending,
+    InProgress,
+    Ended,
+    Completed,
+}
+
+#[derive(Accounts)]
+#[instruction(lottery_id: u64, bump: u8)]
+pub struct InitializeLottery<'info> {
+    #[account(init, payer = authority, space = 8 + LotteryState::INIT_SPACE, seeds = [b"lottery_state", lottery_id.to_le_bytes().as_ref()], bump)]
+    pub lottery_state: Account<'info, LotteryState>,
+    #[account(init, payer = authority, space = 8 + LotteryVault::INIT_SPACE, seeds = [b"lottery_vault", lottery_id.to_le_bytes().as_ref()], bump)]
+    pub lottery_vault: Account<'info, LotteryVault>,
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(deposit_amount: u64)]
+pub struct BuyTickets<'info> {
+    #[account(mut)]
+    pub lottery_state: Account<'info, LotteryState>,
+    #[account(mut)]
+    pub lottery_vault: Account<'info, LotteryVault>,
+    #[account(
+        seeds = [
+            b"user_ticket_data",
+            lottery_state.lottery_id.to_le_bytes().as_ref(),
+            user.key().as_ref()
+        ],
+        bump,
+        init_if_needed,
+        payer = user,
+        space = 8 + TicketData::INIT_SPACE
+    )]
+    pub ticket_data: Account<'info, TicketData>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
 
 #[program]
-pub mod poolpals {
+pub mod lottery {
     use super::*;
 
-    pub fn initialize(ctx: Context<InitializeLottery>, authority: Pubkey) -> Result<()> {
-        // Check if the provided authority matches the authorized initiator's public key
-        let authorized_initiator = Pubkey::try_from(AUTHORIZED_INITIATOR)
-            .map_err(|_| ErrorCode::InvalidAuthorizedInitiator)?;
-        if authority != authorized_initiator {
-            return Err(ErrorCode::UnauthorizedInitiator.into());
-        }
+    pub fn initialize_lottery_state(
+        ctx: Context<InitializeLottery>,
+        lottery_id: u64,
+        start_time: i64,
+        end_time: i64,
+        max_tickets: u64,
+        ticket_price: u64,
+    ) -> Result<()> {
+        // Initialize the lottery state account
+        let lottery_state = &mut ctx.accounts.lottery_state;
+        lottery_state.authority = *ctx.accounts.authority.key;
+        lottery_state.lottery_id = lottery_id;
+        lottery_state.status = LotteryStatus::Pending;
+        lottery_state.start_time = start_time;
+        lottery_state.end_time = end_time;
+        lottery_state.max_tickets = max_tickets;
+        lottery_state.ticket_price = ticket_price;
+        lottery_state.prize_pool = 0;
+        lottery_state.ticket_count = 0;
+        lottery_state.winner = None;
 
-        let lottery = &mut ctx.accounts.lottery;
-
-        lottery.is_initalized = true;
-        lottery.is_finalized = false;
-        lottery.lottery_id = 0;
-        lottery.total_entires = 0;
-        lottery.winning_number = 0;
-        lottery.bump = *ctx.bumps.get("lottery").unwrap();
-
-        msg!("{:?}", lottery);
+        // Initialize the lottery vault account
+        let lottery_vault = &mut ctx.accounts.lottery_vault;
+        lottery_vault.lottery_id = lottery_id;
+        lottery_vault.authority = *ctx.accounts.authority.key;
 
         Ok(())
     }
 
-    // pub fn stake_tokens(ctx: Context<StakeTokens>) -> Result<()> {
-    //     //
+    pub fn buy_tickets(ctx: Context<BuyTickets>, deposit: u64) -> Result<()> {
+        let lottery_state = &mut ctx.accounts.lottery_state;
+        let ticket_data = &mut ctx.accounts.ticket_data;
 
-    //     Ok(())
-    // }
+        msg!("ctx.accounts.user.key: {}", ctx.accounts.user.key);
+        msg!("deposit: {}", deposit);
+        msg!("lottery_state: {:?}", lottery_state);
+        msg!("before ticket_data: {:?}", ticket_data);
 
-    // pub fn store_winning_numbers(ctx: Context<StoreWinningNumbers>) -> Result<()> {
-    //     //
+        // fix this
+        if ticket_data.lottery_id == 0 {
+            ticket_data.lottery_id = lottery_state.lottery_id;
+            ticket_data.user = *ctx.accounts.user.key;
+            ticket_data.ticket_count = 0;
+        }
 
-    //     Ok(())
-    // }
+        msg!("after ticket_data: {:?}", ticket_data);
 
-    // pub fn reward_winners(ctx: Context<RewardWinners>) -> Result<()> {
-    //     //
+        // if ticket_data.lottery_id != lottery_state.lottery_id {
+        //     return Err(LotteryError::InvalidLotteryId.into());
+        // }
 
-    //     Ok(())
-    // }
+        // // if lottery_state.status != LotteryStatus::InProgress {
+        // //     return Err(LotteryError::LotteryNotInProgress.into());
+        // // }
+
+        // if deposit % lottery_state.ticket_price != 0 {
+        //     return Err(LotteryError::InvalidDeposit.into());
+        // }
+
+        // let user_balance = ctx.accounts.user.lamports();
+        // if user_balance < deposit {
+        //     return Err(LotteryError::InsufficientFunds.into());
+        // }
+
+        // let ticket_count = deposit
+        //     .checked_div(lottery_state.ticket_price)
+        //     .ok_or(LotteryError::Overflow)?;
+        // if lottery_state
+        //     .ticket_count
+        //     .checked_add(ticket_count)
+        //     .ok_or(LotteryError::Overflow)?
+        //     > lottery_state.max_tickets
+        // {
+        //     return Err(LotteryError::InsufficientRemainingTickets.into());
+        // }
+
+        // ticket_data.ticket_count = ticket_data
+        //     .ticket_count
+        //     .checked_add(ticket_count)
+        //     .ok_or(LotteryError::Overflow)?;
+
+        // lottery_state.ticket_count = lottery_state
+        //     .ticket_count
+        //     .checked_add(ticket_count)
+        //     .ok_or(LotteryError::Overflow)?;
+        // lottery_state.prize_pool = lottery_state
+        //     .prize_pool
+        //     .checked_add(deposit)
+        //     .ok_or(LotteryError::Overflow)?;
+
+        // ticket_data.total_deposited = ticket_data
+        //     .total_deposited
+        //     .checked_add(deposit)
+        //     .ok_or(LotteryError::Overflow)?;
+
+        // Transfer lamports to the lottery vault
+        // let transfer_instruction = anchor_lang::solana_program::system_instruction::transfer(
+        //     ctx.accounts.user.key,
+        //     &ctx.accounts.lottery_vault.key(),
+        //     deposit,
+        // );
+
+        // let accounts = [
+        //     ctx.accounts.user.to_account_info(),
+        //     ctx.accounts.lottery_vault.to_account_info(),
+        //     ctx.accounts.system_program.to_account_info(),
+        // ];
+
+        msg!("CTX USER: {:?}", ctx.accounts.user.to_account_info());
+        msg!(
+            "CTX VAULT: {:?}",
+            ctx.accounts.lottery_vault.to_account_info()
+        );
+        msg!(
+            "CTX SYS: {:?}",
+            ctx.accounts.system_program.to_account_info()
+        );
+
+        // anchor_lang::solana_program::program::invoke_signed(
+        //     &transfer_instruction,
+        //     &accounts,
+        //     &[&[
+        //         b"user_ticket_data",
+        //         ctx.accounts.lottery_state.lottery_id.to_le_bytes().as_ref(),
+        //         ctx.accounts.user.key.as_ref(),
+        //     ]],
+        // )?;
+
+        Ok(())
+    }
 }
 
-// accounts
-
-#[derive(Accounts)]
-pub struct InitializeLottery<'info> {
-    #[account(
-      init,
-      payer = initializer,
-      space = 8 + Lottery::INIT_SPACE,
-      constraint = !lottery.is_initalized @ ProgramError::AccountAlreadyInitialized
-    )]
-    pub lottery: Account<'info, Lottery>,
-    #[account(
-      init,
-      payer = initializer,
-      space = 8 + GlobalStake::INIT_SPACE, seeds = [b"global_stake"], bump)]
-    pub global_stake: Account<'info, GlobalStake>,
-    #[account(mut)]
-    pub initializer: Signer<'info>,
-    pub system_program: Program<'info, System>,
-}
-
-// #[derive(Accounts)]
-// pub struct StakeTokens<'info> {
-//     #[account(mut)]
-//     pub lottery: Account<'info, Lottery>,
-//     //global stake account
-//     #[account(mut)]
-//     /// CHECK:
-//     pub global_stake: AccountInfo<'info>,
-//     #[account(init, seeds = [b"user_stake", payer.key().as_ref()], bump, payer = payer, space = 8 + 1+32+8)]
-//     //user stake account
-//     /// CHECK:
-//     pub user_stake: AccountInfo<'info>,
-//     #[account(mut)]
-//     pub payer: Signer<'info>,
-//     /// CHECK:
-//     pub system_program: Program<'info, System>,
-// }
-
-// state
-
-#[account]
-#[derive(InitSpace, Debug)]
-pub struct Lottery {
-    pub bump: u8,
-    pub is_initalized: bool,
-    pub is_finalized: bool,
-    pub lottery_id: u32,
-    pub total_entires: u32,
-    pub winning_number: u64,
-}
-
-#[account]
-#[derive(InitSpace, Debug)]
-pub struct GlobalStake {
-    pub stake_mint: Option<Pubkey>,
-    pub stake_amount: u64,
-    pub bump: u8,
-}
-
-#[account]
-#[derive(InitSpace, Debug)]
-pub struct UserStake {
-    pub stake_mint: Option<Pubkey>,
-    pub stake_amount: u64,
+#[error_code]
+pub enum LotteryError {
+    #[msg("Invalid lottery ID.")]
+    InvalidLotteryId,
+    #[msg("Lottery is not in progress.")]
+    LotteryNotInProgress,
+    #[msg("Invalid deposit.")]
+    InvalidDeposit,
+    #[msg("Insufficient funds.")]
+    InsufficientFunds,
+    #[msg("Overflow.")]
+    Overflow,
+    #[msg("Vault already initialized.")]
+    VaultAlreadyInitialized,
+    #[msg("Insufficient remaining tickets.")]
+    InsufficientRemainingTickets,
+    #[msg("Unauthorized.")]
+    Unauthorized,
+    #[msg("Invalid status transition.")]
+    InvalidStatusTransition,
 }
--- tests/poolpals.ts
+++ tests/poolpals.ts
@@ -1,39 +1,238 @@
-import * as anchor from "@project-serum/anchor";
+// Import necessary libraries and components
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+const { SystemProgram, PublicKey, Keypair, LAMPORTS_PER_SOL } = anchor.web3;
 import { expect } from "chai";
-import { Program } from "@project-serum/anchor";
-import { Poolpals } from "../target/types/poolpals";
-const { SystemProgram } = anchor.web3;
+import { Lottery } from "../target/types/lottery";
 
-describe("poolpals", () => {
+// Define the sleep function to add a delay
+const sleep = async (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+// Define the airdropSOL function to airdrop SOL to the specified account
+async function airdropSOL(program, accountPublicKey, solAmount) {
+  const lamports = LAMPORTS_PER_SOL * solAmount;
+  await program.provider.connection.requestAirdrop(accountPublicKey, lamports);
+}
+
+// Main test suite for the Lottery program
+describe("Lottery", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
+  const program = anchor.workspace.Lottery as Program<Lottery>;
+  const provider = anchor.AnchorProvider.env();
 
-  const program = anchor.workspace.Poolpals as Program<Poolpals>;
+  // Define variables to be used throughout the tests
+  let authorityAccount, vaultAccount, authority, vault, user, userAccount;
 
-  it("Is initialized!", async () => {
-    const lottoAccount = await anchor.web3.Keypair.generate();
+  let lotteryId, lotteryStateAddress;
 
-    let [globalStakePDA, _] = anchor.web3.PublicKey.findProgramAddressSync(
-      [Buffer.from("global_stake")],
-      program.programId
+  // Set up the test environment before running the tests
+  before(async () => {
+    // Generate keypairs for the authority, vault, and user accounts
+    authority = Keypair.generate();
+    authorityAccount = authority.publicKey;
+
+    vault = Keypair.generate();
+    vaultAccount = vault.publicKey;
+
+    user = Keypair.generate();
+    userAccount = user.publicKey;
+
+    // Airdrop 10 SOL to the authority, vault, user, and program accounts
+    await airdropSOL(program, authorityAccount, 10);
+    await airdropSOL(program, vaultAccount, 10);
+    await airdropSOL(program, user.publicKey, 10);
+    await airdropSOL(program, program.programId, 10);
+
+    console.log(
+      `Authority account: ${authorityAccount.toString()} (https://explorer.solana.com/address/${authorityAccount.toString()}?cluster=custom&customUrl=http%3A%2F%2Flocalhost%3A8899)`
     );
 
-    const tx = await program.methods
-      .initialize()
-      .accounts({
-        lottery: program.provider.publicKey,
-        globalStake: globalStakePDA,
-        initializer: lottoAccount.publicKey,
-        systemProgram: SystemProgram.programId,
-      })
-      .signers([lottoAccount])
-      .rpc();
+    console.log(
+      `Vault account: ${vaultAccount.toString()} (https://explorer.solana.com/address/${vaultAccount.toString()}?cluster=custom&customUrl=http%3A%2F%2Flocalhost%3A8899)`
+    );
 
-    console.log(tx);
+    console.log(
+      `User account: ${user.publicKey.toString()} (https://explorer.solana.com/address/${user.publicKey.toString()}?cluster=custom&customUrl=http%3A%2F%2Flocalhost%3A8899)`
+    );
 
-    // const account = await program.account.lottery.fetch(lottoAccount.publicKey);
+    // Wait for 3 seconds for the airdrops to complete
+    await sleep(3000);
+  });
 
-    // // expect(account.isInitalized == true);
-    // expect(account.isInitalized).to.be.true;
+  // Test suite for initializing the lottery state
+  describe("Initialize Lottery State", () => {
+    it("should initialize a new lottery state", async () => {
+      // Generate a random lottery ID and define other parameters for the lottery state
+      lotteryId = new anchor.BN(
+        Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)
+      );
+      const ticketPrice = new anchor.BN(Math.round(0.01 * LAMPORTS_PER_SOL));
+      const startTime = new anchor.BN(
+        Math.floor(new Date().getTime() / 1000 + 60)
+      );
+      const endTime = new anchor.BN(startTime.toNumber() + 60 * 60);
+      const maxTickets = new anchor.BN(1000);
+
+      // Create the lottery state account with PDA
+      const lotteryStateSeeds = [
+        Buffer.from("lottery_state"),
+        new anchor.BN(lotteryId).toArrayLike(Buffer, "le", 8),
+      ];
+
+      let [lotteryStateAddress, lotteryStateBump] =
+        await PublicKey.findProgramAddressSync(
+          lotteryStateSeeds,
+          program.programId
+        );
+
+      // Create the lottery vault account with PDA
+      const lotteryVaultSeeds = [
+        Buffer.from("lottery_vault"),
+        new anchor.BN(lotteryId).toArrayLike(Buffer, "le", 8),
+      ];
+
+      let [lotteryVaultAddress, lotteryVaultBump] =
+        await PublicKey.findProgramAddressSync(
+          lotteryVaultSeeds,
+          program.programId
+        );
+
+      // Initialize the lottery state using the `initializeLotteryState` method
+      try {
+        await program.methods
+          .initializeLotteryState(
+            lotteryId,
+            startTime,
+            endTime,
+            maxTickets,
+            ticketPrice
+          )
+          .accounts({
+            authority: authority.publicKey,
+            lotteryState: lotteryStateAddress,
+            lotteryVault: lotteryVaultAddress,
+            systemProgram: SystemProgram.programId,
+          })
+          .signers([authority])
+          .rpc();
+      } catch (error) {
+        console.error("Error in initializeLotteryState:", error);
+      }
+
+      // Fetch the created lottery state account
+      const lotteryState = await program.account.lotteryState.fetch(
+        lotteryStateAddress
+      );
+
+      // Assert that the lottery state account was initialized with the correct data
+      expect(lotteryState.ticketPrice.eq(ticketPrice)).to.be.true;
+      expect(lotteryState.startTime.eq(startTime)).to.be.true;
+      expect(lotteryState.endTime.eq(endTime)).to.be.true;
+      expect(lotteryState.maxTickets.eq(maxTickets)).to.be.true;
+
+      console.log(`Lottery ID: ${lotteryId.toString()}`);
+
+      console.log(
+        `Lottery state address: ${lotteryStateAddress.toString()} (https://explorer.solana.com/address/${lotteryStateAddress.toString()}?cluster=custom&customUrl=http%3A%2F%2Flocalhost%3A8899)`
+      );
+
+      lotteryStateAddress = lotteryStateAddress;
+    });
+  });
+
+  // Test suite for buying lottery tickets
+  describe("Buy Tickets", () => {
+    it("should allow user to buy 1 SOL worth of lottery tickets", async () => {
+      // Set the desired amount of SOL to buy in tickets
+      const solAmount = 1;
+      const ticketPrice = new anchor.BN(Math.round(0.05 * LAMPORTS_PER_SOL));
+      const ticketCount = new anchor.BN(solAmount * 1); // 1 SOL = 80 tickets at 0.05 SOL per ticket
+
+      console.log("ticketPrice: ", ticketPrice.toString());
+      console.log("Desired ticket count:", ticketCount.toString());
+
+      // Calculate the deposit amount
+      const depositAmount = ticketCount.div(ticketPrice);
+
+      console.log("depositAmount:", depositAmount.toString());
+
+      // Create the lottery state account with PDA
+      const lotteryStateSeeds = [
+        Buffer.from("lottery_state"),
+        new anchor.BN(lotteryId).toArrayLike(Buffer, "le", 8),
+      ];
+
+      let [lotteryStateAddress, lotteryStateBump] =
+        await PublicKey.findProgramAddressSync(
+          lotteryStateSeeds,
+          program.programId
+        );
+
+      // Calculate the PDA for the user's ticket data
+      const ticketDataSeeds = [
+        Buffer.from("user_ticket_data"),
+        new anchor.BN(lotteryId).toArrayLike(Buffer, "le", 8),
+        userAccount.toBuffer(),
+      ];
+      const [ticketDataAddress, ticketDataBump] =
+        await PublicKey.findProgramAddressSync(
+          ticketDataSeeds,
+          program.programId
+        );
+
+      // Create the lottery vault account with PDA
+      const lotteryVaultSeeds = [
+        Buffer.from("lottery_vault"),
+        new anchor.BN(lotteryId).toArrayLike(Buffer, "le", 8),
+      ];
+
+      let [lotteryVaultAddress, lotteryVaultBump] =
+        await PublicKey.findProgramAddressSync(
+          lotteryVaultSeeds,
+          program.programId
+        );
+
+      // Buy tickets using the `buyTickets` method
+      try {
+        await program.methods
+          .buyTickets(ticketCount)
+          .accounts({
+            lotteryState: lotteryStateAddress,
+            lotteryVault: lotteryVaultAddress,
+            ticketData: ticketDataAddress,
+            user: userAccount,
+            systemProgram: SystemProgram.programId,
+          })
+          .signers([user])
+          .rpc();
+      } catch (error) {
+        console.error("Error in buyTickets:", error);
+      }
+
+      // Fetch the updated user ticket data account
+      const userTicketData = await program.account.ticketData.fetch(
+        ticketDataAddress
+      );
+
+      console.log("User ticket data:", userTicketData);
+
+      // Fetch the updated lottery state account
+      const updatedLotteryState = await program.account.lotteryState.fetch(
+        lotteryStateAddress
+      );
+
+      console.log("Updated lottery state:", updatedLotteryState);
+
+      // Add assertions to verify if the user has successfully bought the tickets
+      expect(userTicketData.ticketCount.eq(ticketCount)).to.be.true;
+
+      // Add assertions to verify if the lottery state has been updated correctly
+      expect(
+        updatedLotteryState.ticketCount.eq(
+          updatedLotteryState.ticketCount.add(ticketCount)
+        )
+      ).to.be.true;
+    });
   });
 });
